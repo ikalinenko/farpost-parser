9a4e7ee602cbcc61a40ea6d637ef08ebdee49f21
ikalinenko <53858660+ikalinenko@users.noreply.github.com>
2023-01-21 14:03:41 +0300

    Logging exception added

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import logging
 from parser import Parser
+
+
+logger = logging.getLogger(__file__)
 
 
 if __name__ == '__main__':
     parser = Parser(base_url='https://www.farpost.ru/user/VLtires/auto/wheel/')
     parser.get_catalog_links()
-    parser.parse_catalog_items()
+
+    try:
+        parser.parse_catalog_items()
+    except Exception as e:
+        logger.exception(e)
+
     parser.dump_disks()
     parser.dump_tires()
