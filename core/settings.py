529867ce71e323a3f8401c2a06f9e1528b21dc61
ikalinenko <53858660+ikalinenko@users.noreply.github.com>
2023-01-21 14:03:26 +0300

    Logging to file
    Logging to file was set

--- core/settings.py
+++ core/settings.py
@@ -1,9 +1,12 @@
 import logging
+import os
 from pathlib import Path
 
 
-logging.basicConfig(
-    level=logging.DEBUG
-)
-
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename=os.path.join(BASE_DIR, 'root.log')
+)
