ee917f73eb32c415f2059c2104d6a1b33454db59
ikalinenko <53858660+ikalinenko@users.noreply.github.com>
2023-01-26 21:53:07 +0300

    Bugs fixed

--- helpers/parse_html.py
+++ helpers/parse_html.py
@@ -259,8 +259,16 @@ def parse_disk(content_to_parse: str) -> Disk:
     diameter = soup.find('span', {'data-field': 'wheelDiameter'}).text
 
     disk_parameters = soup.find('div', {'data-field': 'discParameters'}).select('div.value')
-    disc_width = disk_parameters[0].text
-    departure_ET = disk_parameters[1].text
+
+    try:
+        disc_width = disk_parameters[0].text
+    except IndexError:
+        disc_width = None
+
+    try:
+        departure_ET = disk_parameters[1].text
+    except IndexError:
+        departure_ET = None
 
     drilling_PCD = soup.find('span', {'data-field': 'wheelPcd'}).text
     type_of = soup.find('span', {'data-field': 'diskType'}).text
