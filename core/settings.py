ecc39479d5185e8e6074505da35c2f6523213a04
ikalinenko <53858660+ikalinenko@users.noreply.github.com>
2023-01-26 18:21:28 +0300

    Environment variables added
    Environment variables added & logging formatting changed

--- core/settings.py
+++ core/settings.py
@@ -1,13 +1,20 @@
 import logging
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR)
+
+RUCAPTCHA_API_KEY = os.environ.get('RUCAPTCHA_API_KEY')
+GOOGLE_SITE_KEY = os.environ.get('GOOGLE_SITE_KEY')
+
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(levelname) %(asctime) - %(message)',
+    format='{levelname} {asctime} - {message}',
+    style='{',
     filename=os.path.join(BASE_DIR, 'root.log')
 )
