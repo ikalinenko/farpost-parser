8794effbb09e48919921605776f003225445d492
ikalinenko <53858660+ikalinenko@users.noreply.github.com>
2023-01-26 21:28:21 +0300

    Bug fixed

--- helpers/parse_html.py
+++ helpers/parse_html.py
@@ -191,14 +191,18 @@ def parse_tire(content_to_parse: str) -> Tire:
     soup = BeautifulSoup(content_to_parse, features='html.parser')
 
     title = soup.find('span', {'data-field': 'subject'}).text
-    price_for_set = soup.find('span', {'data-field': 'price'}).get('data-bulletin-price')
+
+    try:
+        price_for_set = soup.find('span', {'data-field': 'price'}).get('data-bulletin-price')
+    except AttributeError:
+        price_for_set = None
 
     try:
         the_number_of_tires_in_an_indivisible_set = soup.find('span', {'data-field': 'inSetQuantity'}).text
     except AttributeError:
         the_number_of_tires_in_an_indivisible_set = None
 
-    if the_number_of_tires_in_an_indivisible_set:
+    if price_for_set and the_number_of_tires_in_an_indivisible_set:
         price = str(int(price_for_set) / _get_integer_from_string(the_number_of_tires_in_an_indivisible_set))
     else:
         price = price_for_set
